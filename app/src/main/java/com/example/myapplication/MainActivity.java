5bb5318d3291e63db7c86544a9e0f65ceaf5525d
olsson22 <olssonup.edu>
2019-09-12 08:20:28 -0700

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //hi there
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
